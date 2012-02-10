73a5398b71b584764d9eca82843f353fb1fcdc1d
Dimitri Glazkov <dglazkov@chromium.org>
2012-02-10 13:59:48 -0800

    Added a touch of style.

--- tabs-control.js
+++ tabs-control.js
@@ -4,13 +4,14 @@
 var OVERFLOW_LIMIT = 6;
 
 // FIXME: Most of this inline style cruft should be unnecssary once scoped styles are available.
-var CURRENT_TAB_STYLE = 'background-color: #333; color: white; -webkit-user-select: none';
-var TAB_STYLE = 'font-size: medium; font-weight: normal; margin: 0 2px 0 0; padding: 5px 15px; text-align: center; text-overflow: ellipsis; overflow: hidden; cursor: pointer; -webkit-user-select: none';
+var CURRENT_TAB_STYLE = 'background: white; color: black; -webkit-user-select: none; text-shadow: 0 0 1px black';
+var TAB_STYLE = 'font-family: \'Open Sans\'; font-size: 16px; line-height: 22px; font-weight: 400; padding: 5px 15px; margin: 0 5px 0 0; text-align: center; text-overflow: ellipsis; overflow: hidden; cursor: pointer; -webkit-user-select: none; border-top-left-radius: 6px; border-top-right-radius: 6px; background: rgb(242, 242, 242); color: rgb(64, 64, 64); box-shadow: 0 2px 2px rgba(0, 0, 0, .3);';
 var MORE_STYLE = '';
-var TAB_STRIP_STYLE = 'display: -webkit-flexbox; -webkit-flex-pack: justify; padding: 0 0 2px 0; -webkit-user-select: none';
-var CURRENT_CONTENTS_STYLE = 'border: 1px solid #ccc; padding: 10px; overflow: auto; height: 600px';
-var CONTAINER_STYLE = 'display: -webkit-flexbox; -webkit-flex-direction: column; -webkit-flex-align: stretch;';
-var HELP_STYLE = 'background-color: #eee; padding: 5px 10px; margin: 2px 0 0 0; font-size: small;';
+var TAB_STRIP_STYLE = 'display: -webkit-flexbox; -webkit-flex-pack: justify; padding: 0 5px; -webkit-user-select: none;';
+var TAB_WRAPPER_STYLE = 'display: -webkit-flexbox; overflow: hidden; height: 30px;';
+var CURRENT_CONTENTS_STYLE = 'box-shadow: 0 2px 2px rgba(0, 0, 0, .3); background: white; border-radius: 3px; padding: 5px 20px; font-family: \'Open Sans\'; font-size: 14px; line-height: 20px; overflow: auto; height: 600px';
+var CONTAINER_STYLE = 'display: -webkit-flexbox; -webkit-flex-direction: column; -webkit-flex-align: stretch; background: -webkit-linear-gradient(rgb(230, 230, 230), rgb(200, 200, 200)); padding: 10px; -webkit-font-smoothing: antialiased;';
+var HELP_STYLE = 'background-color: #eee; padding: 5px 10px; margin: 2px 0 0 0; font-family: \'Open Sans\'; font-size: 11px; border-radius: 3px; box-shadow: 0 2px 2px rgba(0, 0, 0, .3);';
 var OVERFLOW_STYLE = 'position: relative';
 var OVERFLOW_TAB_STRIP_STYLE_CLOSED = 'display:none';
 var OVERFLOW_TAB_STRIP_STYLE_OPEN = 'position:absolute; background: #fff; border: 1px solid #999; padding: 2px 0 2px 2px; right: 0; width: 300px';
@@ -21,14 +22,17 @@ var HTML_TEMPLATE = [
     '<style>',
         'h2:nth-of-type(1) {', CURRENT_TAB_STYLE, '} ', 
         'h2 {', TAB_STYLE, '}',
-        'h2:nth-of-type(n+', OVERFLOW_LIMIT, ') { text-align: left }',
-        '.more { color: red }',
+        'h2:nth-of-type(n+', OVERFLOW_LIMIT + 1, ') { text-align: left; box-shadow: 0; border-radius: 0; background: white; }',
     '</style>',
     '<div style="', CONTAINER_STYLE, '">',
         '<div style="', TAB_STRIP_STYLE, '">',
-            '<content class="strip" select="h2:nth-of-type(-n+', OVERFLOW_LIMIT, ')"></content>',
+            '<div style="', TAB_WRAPPER_STYLE, ';width: 80%;">',
+               '<content class="strip" select="h2:nth-of-type(-n+', OVERFLOW_LIMIT, ')"></content>',
+            '</div>',
             '<div class="overflow" style="', OVERFLOW_STYLE, '">',
-                '<div class="more" style="', TAB_STYLE, MORE_STYLE, '">&hellip;</div>',
+                '<div style="', TAB_WRAPPER_STYLE, '">',
+                    '<div class="more" style="', TAB_STYLE, MORE_STYLE, '">&hellip;</div>',
+                '</div>',
                 '<div style="', OVERFLOW_TAB_STRIP_STYLE_CLOSED, '">',
                     '<content class="overflow" select="h2:nth-of-type(n+', OVERFLOW_LIMIT, ')"></content>',
                 '</div>',
@@ -54,8 +58,10 @@ function TabsControl(host)
     this.moreOpened = false;
     this.style = container.querySelector('style').sheet;
     this.current = container.querySelector('content.current');
-    this.overflow = container.querySelector('.overflow');
-    this.overflow.addEventListener('click', this.onClickOverflow.bind(this));
+    var overflow = container.querySelector('.overflow');
+    overflow.addEventListener('click', this.onClickOverflow.bind(this));
+    this.more = overflow.querySelector('.more');
+    this.menu = overflow.lastElementChild;
     this.updateCurrent();
     this.root.host.addEventListener('click', this.onClickTab.bind(this));
     window.addEventListener('click', this.onClickWindow.bind(this));
@@ -83,13 +89,13 @@ TabsControl.prototype = {
         }
     },
     openMore: function() {
-        this.overflow.firstElementChild.style.cssText = CURRENT_TAB_STYLE + TAB_STYLE;
-        this.overflow.lastElementChild.style.cssText = OVERFLOW_TAB_STRIP_STYLE_OPEN;
+        this.more.style.cssText = CURRENT_TAB_STYLE + TAB_STYLE;
+        this.menu.style.cssText = OVERFLOW_TAB_STRIP_STYLE_OPEN;
         this.moreOpened = true;
     },
     closeMore: function()  {
-        this.overflow.firstElementChild.style.cssText = TAB_STYLE;
-        this.overflow.lastElementChild.style.cssText = OVERFLOW_TAB_STRIP_STYLE_CLOSED;
+        this.more.style.cssText = TAB_STYLE;
+        this.menu.style.cssText = OVERFLOW_TAB_STRIP_STYLE_CLOSED;
         this.moreOpened = false;
     },
     onClickOverflow: function(evt) {
